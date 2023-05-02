ffc2c8eb0e3e5dfd831c689cc94f981507dd10e9
nahidhasan141400 <nahidhasan141400@gmail.com>
2023-05-02 16:30:49 +0600

    add new

--- pages/Ticket/index.jsx
+++ pages/Ticket/index.jsx
@@ -1,10 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 import Layout from "../../components/Layout";
 
 const Index = () => {
   return (
     <Layout>
-        <div>Index</div>
+        <div className='w-full h-screen flex justify-center items-center'>
+          <div className='text-center'>
+            <h1 className='text-2xl capitalize mb-2'>Our E-ticketing Service is Offline Now</h1>
+            <p className='font-light italic capitalize'> contact us for get ticket service</p>
+            <Link href={'/contact'}>
+              <button className='btn btn-primary mt-8'> Contact With Us </button>
+            </Link>
+          </div>
+        </div>
     </Layout>
   )
 }
